3d13120b56e3d27a146317a1d280fd34ea1b422c
Henk Reder <henk@yetanalytics.com>
2026-08-15 18:24:26 -0400

    got rid of legacy vc scripts

--- deck_hid.py
+++
@@ -1,9 +0,0 @@
-from importlib import import_module
-import sys
-
-from runtime_bootstrap import ensure_runtime_src_path
-
-
-ensure_runtime_src_path()
-_impl = import_module("decktation_runtime.hid")
-sys.modules[__name__] = _impl
