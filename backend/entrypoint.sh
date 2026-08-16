8fc801fe31177da73603c0f3948604dd859c0882
Henk Reder <henk@yetanalytics.com>
2026-08-15 22:39:55 -0400

    removed portaudio from refs in Dockerfile

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,9 +2,7 @@
 set -eu
 
 cd /backend
-mkdir -p out/licenses out/lib
+mkdir -p out/licenses
 cp /runtime-build/runtime/dist/decktation-runtime out/decktation-runtime
 chmod 755 out/decktation-runtime
-cp -L /usr/lib/libportaudio.so.2 out/lib/libportaudio.so.2
 cp /ydotool-src/LICENSE out/licenses/ydotool-AGPL-3.0.txt
-cp /usr/share/licenses/portaudio/LICENSE.txt out/licenses/portaudio-MIT.txt
